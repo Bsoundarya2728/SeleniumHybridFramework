0124cb744f62261201ba16f315dc202dac63165a
Bsoundarya <b.soundarya1123@gmail.com>
2024-09-27 16:53:20 +0530

    adding Restassured prjct

--- eclipse-workspace/RestAssured_Testing/src/test/java/requests/HTTPRequests.java
+++ eclipse-workspace/RestAssured_Testing/src/test/java/requests/HTTPRequests.java
@@ -5,6 +5,8 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 
 /*
  given()
@@ -16,8 +18,9 @@ import static org.hamcrest.Matchers.*;
  add() - is also part of then(), if we want use we can use add()
  */
 public class HTTPRequests {
+	int id;
 	
-	@Test
+	@Test(priority=1)
 	void getUser()
 	{
 		given()
@@ -29,6 +32,52 @@ public class HTTPRequests {
 			.statusCode(200)
 			.body("page",equalTo(2))
 			.log().all();
+	}
+	@Test(priority=2)
+	void createUser()
+	{
+		HashMap data=new HashMap();
+		data.put("name", "Soudnarya");
+		data.put("Job", "Senior QA");
+		data.put("Location", "Noida-Unit1");
+		
+		id= given()
+			.contentType("application/json")
+			.body(data)
+		.when()
+			.post("https://reqres.in/api/users")
+			.jsonPath().getInt("id")
+		/*.then()
+			.statusCode(201)
+			.log().all()*/
+			;
+	}
+	@Test(priority=3, dependsOnMethods  ={"createUser"})
+	void updateUser()
+	{
+		HashMap data=new HashMap();
+		data.put("name", "BejjenkiSoudnarya");
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+		.when()
+			.put("https://reqres.in/api/users/"+id)
+		.then()
+			.statusCode(200)
+			.log().all();
+		
+	}
+	@Test(priority=4)
+	void deleteUser()
+	{
+		given()
+		.when()
+		.delete("https://reqres.in/api/users/"+id)
+		.then()
+		.statusCode(204)
+		.log().all()
+		;
 	}
 	
 }
